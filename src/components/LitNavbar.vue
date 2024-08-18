ab7a0b2e63305192fee77ce656ab1cdfa398eae1
giffarialfarizy <giffari.alfarizy@xtremax.com>
2024-08-18 17:48:57 +0700

    ♻️ Change class naming with BEM with single underscore and double dash

--- src/components/LitNavbar.vue
+++ src/components/LitNavbar.vue
@@ -1,58 +1,60 @@
 <template>
   <nav class="navbar">
     <div class="container-fluid">
-      <RouterLink class="navbar-brand" to="/">Navbar</RouterLink>
+      <RouterLink class="navbar_brand" to="/">Navbar</RouterLink>
       <!-- Toggle button -->
       <button
-        class="navbar-toggler"
+        class="navbar_toggler"
         type="button"
         @click="toggleNavbar"
         aria-controls="navbarSupportedContent"
         :aria-expanded="isCollapsed ? 'false' : 'true'"
         aria-label="Toggle navigation"
       >
-        <span class="navbar-toggler-icon"> </span>
+        <span class="navbar_toggler_icon"></span>
       </button>
       <!-- Toggle this element on mobile view -->
-      <div :class="['navbar-collapse', { show: !isCollapsed }]" id="navbarSupportedContent">
-        <ul class="navbar-nav">
-          <li class="nav-item">
-            <RouterLink class="nav-link" :class="{ active: isActive('/') }" to="/">
+      <div :class="['navbar_collapse', { show: !isCollapsed }]" id="navbarSupportedContent">
+        <ul class="navbar_nav">
+          <li class="navbar_nav_item">
+            <RouterLink class="navbar_nav_link" :class="{ active: isActive('/') }" to="/">
               Home
             </RouterLink>
           </li>
-          <li class="nav-item">
-            <RouterLink class="nav-link" :class="{ active: isActive('/about') }" to="/about">
+          <li class="navbar_nav_item">
+            <RouterLink class="navbar_nav_link" :class="{ active: isActive('/about') }" to="/about">
               About
             </RouterLink>
           </li>
           <li
-            class="nav-item dropdown"
+            class="navbar_nav_item dropdown"
             @mouseenter="toggleDropdown(true)"
             @mouseleave="toggleDropdown(false)"
           >
             <button
-              class="nav-link dropdown-toggle"
+              class="navbar_nav_link dropdown_toggle"
               role="button"
               :aria-expanded="dropdownOpen ? 'true' : 'false'"
             >
               Dropdown
             </button>
-            <ul class="dropdown-menu" :class="{ show: dropdownOpen }">
-              <li><RouterLink class="dropdown-item" to="/action">Page A</RouterLink></li>
+            <ul class="dropdown_menu" :class="{ show: dropdownOpen }">
+              <li><RouterLink class="dropdown_item" to="/action">Page A</RouterLink></li>
               <li>
-                <RouterLink class="dropdown-item" to="/another-action">Page B</RouterLink>
+                <RouterLink class="dropdown_item" to="/another-action">Page B</RouterLink>
               </li>
-              <li><hr class="dropdown-divider" /></li>
+              <li><hr class="dropdown_divider" /></li>
               <li>
-                <RouterLink class="dropdown-item" to="/something-else"
-                  >Something else here</RouterLink
-                >
+                <RouterLink class="dropdown_item" to="/something-else">
+                  Something else here
+                </RouterLink>
               </li>
             </ul>
           </li>
-          <li class="nav-item">
-            <RouterLink class="nav-link disabled" aria-disabled="true" to="#">Disabled</RouterLink>
+          <li class="navbar_nav_item">
+            <RouterLink class="navbar_nav_link disabled" aria-disabled="true" to="#">
+              Disabled
+            </RouterLink>
           </li>
         </ul>
         <form class="flex" role="search">
@@ -63,7 +65,7 @@
             aria-label="Search"
             v-model="searchQuery"
           />
-          <button class="btn btn-outline-primary" type="submit">Search</button>
+          <button class="btn btn-outline--primary" type="submit">Search</button>
         </form>
       </div>
     </div>
